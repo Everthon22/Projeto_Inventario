d2bdde1802e2af46db34d4ab5ed7b1de997b81c5
Everthon <everthonbernardes338@gmail.com>
2025-05-07 10:19:30 -0300

    Add files via upload

---
+++ inventario.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 07/05/2025 às 15:18
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `inventario`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `item`
+--
+
+CREATE TABLE `item` (
+  `id` int(11) NOT NULL,
+  `nome_item` varchar(255) NOT NULL,
+  `qtd_item` int(11) NOT NULL,
+  `img_item` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `item`
+--
+
+INSERT INTO `item` (`id`, `nome_item`, `qtd_item`, `img_item`) VALUES
+(1, 'Espada Mestre', 1, 'assets1/img1/espada_mestre.jpg'),
+(2, 'Escudo Hyliano', 1, 'assets1/img1/escudo.jpeg'),
+(3, 'Poção de Vida', 1, 'assets1/img1/porcao.png');
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `item`
+--
+ALTER TABLE `item`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
